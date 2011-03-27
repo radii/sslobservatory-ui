ce451504e85ecb6adf791c1f7127f7ee35340599
Andy Isaacson <adi@hexapodia.org>
2011-03-27 02:50:14 -0700

    render_cert: fix up for initial features
    rewrite renderer to .append one string, rather than .append'ing many times,
    and write properly nested div and ul so x.509 works right.
    
    hack CSS to handle "defaulthidden" divs and render the fake [more] button.

--- observatory.js
+++ observatory.js
@@ -16,30 +16,34 @@ function render_cert(src, fp) {
                   'ext:X509v3 Certificate Policies:Policy',
                   'ext:X509v3 CRL Distribution Points',
                  ]
-    $('#debug').append('json=' + json);
+    dbg('json=' + json);
     $.getJSON(src + fp, function(data) {
-            $('#content').append("<h2>Properties for<span id='keynum'> " + data['fingerprint'] +
-                "</span></h2><br>");
             function morsel(title, name, content) {
-                $('#content').append('<li class="' + name + '"><span class="label">'
+                return ('<li class="' + name + '"><span class="label">'
                     + title + ':</span> <span class="' + name + '">' + content + 
                     '</span></li>');
             }
-            $('#content').append('<ul class=>');
-            // stuff about the key
-            morsel('IP', 'ip', data['ip']);
+            $('#content').append("<h2>Properties for<span id='keynum'> " + data['fingerprint'] +
+                "</span></h2><br><ul>" +
+            // stuff about the use of the key
+            morsel('IP', 'ip', data['ip']) +
 
             // key details
-            morsel('Key size', 'modulus_bits', data['RSA_Modulus_Bits']);
-            morsel('Algorithm', 'algorithm', data['Signature Algorithm']);
+            morsel('Key size', 'modulus_bits', data['RSA_Modulus_Bits']) +
+            morsel('Algorithm', 'algorithm', data['Signature Algorithm']) +
+            morsel('RSA modulus', 'modulus', data['RSA Public Key:Modulus']) +
 
             // x509 crap
-            $('#content').append('<li>X.509 data <span class="moreclick" id="x509more">[more]</span><div id="x509opt"><ul>');
-                morsel('X.509 Subject', 'subject', data['Subject']);
-                morsel('Authority Key Identifier', 'akid', data['ext:X509v3 Authority Key Identifier:keyid']);
-                morsel('Certificate Policies:Policy', 'cert_policies', data['ext:X509v3 Certificate Policies:Policy']);
-                morsel('CRL Distribution Points', 'crl', data['ext:X509v3 CRL Distribution Points']);
-            $('#content').append("</ul></div></li>");
+            '<li><span class="label">X.509</span> <span class="moreclick" id="x509more">[more]</span><div class="defaulthidden" id="x509opt"><ul>' +
+                morsel('X.509 Subject', 'subject', data['Subject']) +
+                morsel('Authority Key Identifier', 'akid', data['ext:X509v3 Authority Key Identifier:keyid']) +
+                morsel('Certificate Policies:Policy', 'cert_policies', data['ext:X509v3 Certificate Policies:Policy']) +
+                morsel('CRL Distribution Points', 'crl', data['ext:X509v3 CRL Distribution Points']) +
+            '</ul></div></li>');
+            $('#x509more').click(function() {
+                    $('#x509opt').toggle('fast');
+                    $(self).html("[hide]");
+                    })
         })
 }
 
