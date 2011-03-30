f8e3f1f32bb77e9b9e9c26b6a418e18c55b05b01
Andy Isaacson <adi@hexapodia.org>
2011-03-30 01:33:54 -0700

    clean up fallout from ssl.html refactoring and tidy observatory.js

--- observatory.js
+++ observatory.js
@@ -6,20 +6,8 @@ function render_cert(src, fp) {
     var json = src + fp;
     var pem = json + "/pem";
     var der = json + "/der";
-    var fields = ['Subject', 'RSA_Modulus_Bits', 'fingerprint',
-                  'Issuer', 'Serial Number',
-                  'ext:X509v3 Key Usage', 'ext:X509v3 Extended Key Usage',
-                  'ip', 'fetchtime',
-                  'Signature Algorithm', 'Signature',
-                  'Subject Public Key Info:Public Key Algorithm',
-                  'RSA Public Key:Modulus', 'Subject Public Key Info:RSA Public Key:Exponent',
-                  'Validity:Not Before', 'Validity:Not After',
-                  'ext:X509v3 Authority Key Identifier:keyid',
-                  'ext:X509v3 Certificate Policies:Policy',
-                  'ext:X509v3 CRL Distribution Points',
-                 ]
     dbg('json=' + json);
-    $.getJSON(src + fp, function(data) {
+    $.getJSON(json, function(data) {
             $('.data').each(function(i, m) {
                 var key = $(this).attr('datasrc');
                 console.log("#data each i=" + i + " key=" + key);
